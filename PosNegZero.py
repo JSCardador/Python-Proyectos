7cdf63114e318e1676fe330995637e1e5c4ecdbf
JSCardador <34039920+JSCardador@users.noreply.github.com>
2020-05-03 09:41:23 +0200

    Script PosNegZero

---
+++ PosNegZero.py
@@ -0,0 +1,9 @@
+def PosNegZero(number):
+    if(number>0):
+        print(f"El numero {number} es positivo.")
+    elif (number<0):
+        print(f"El numero {number} es negativo.")
+    elif (number==0):
+        print(f"El numero {number} es cero.")
+        
+PosNegZero(-12)
