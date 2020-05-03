6a4383cf2a75d0ac190a0a4c748b56de5ed06ec9
JSCardador <34039920+JSCardador@users.noreply.github.com>
2020-05-03 09:34:33 +0200

    Script ParOImpar

---
+++ ParOImpar.py
@@ -0,0 +1,13 @@
+def OddOrEven(number):
+    if(number % 2 == 0):
+        print(f"El numbero {number} es par")
+    else:
+        print(f"El numbero {number} es impar")
+
+OddOrEven(23679)
+
+###
+# Logica: Si el modulo de 2 del numero proporcionado es cero
+# el numero será par.
+# En caso contrario sera impar.
+# ###
