60f860ccb6769e264c15b53dd0fdd5c4189a6586
JSCardador <34039920+JSCardador@users.noreply.github.com>
2020-05-03 10:01:25 +0200

    Script Rombo

---
+++ Miniprogramas/DibujarRombo.py
@@ -0,0 +1,9 @@
+def Rombo(n):
+    for i in range(1, n + 1):
+        print(" " *(n-i) + "* " * i)
+    for j in range(1, n):
+        print(" " * j + "* " * (n-j))
+
+
+Rombo(7)
+# n = Numero de lineas del Rombo
