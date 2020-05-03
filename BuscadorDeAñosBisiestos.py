0d9b30da78c801163abb45b2ec550c8ea08040a9
JSCardador <34039920+JSCardador@users.noreply.github.com>
2020-05-03 09:27:09 +0200

    Script Buscador de años bisiestos

---
+++ BuscadorDeAñosBisiestos.py
@@ -0,0 +1,21 @@
+def LeapYear(year):
+    if(year % 4) ==0:
+        if(year % 100) == 0:
+            if(year % 400) == 0:
+                print(f"{year} fue un año bisiesto")
+            else:
+                print(f"{year} no fue un año bisiesto")
+        else:
+            print(f"{year} fue un año bisiesto")
+    else:
+        print(f"{year} no fue un año bisiesto")
+
+LeapYear(1996)
+
+
+###
+# Condiciones para un año sea Bisiesto:
+#   1. Si un año es divisible entre 4, será bisiesto.
+#   2. Si un año es divisible entre 4 pero no entre 100, es bisiesto.
+#   3. Si un año es divisible entre 100 y entre 400, es bisieto.
+# ###
