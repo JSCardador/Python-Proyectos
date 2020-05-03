903025a9a37398f4267816114d25c72827c1a1b3
JSCardador <34039920+JSCardador@users.noreply.github.com>
2020-05-03 09:09:10 +0200

    Script Fibonacci numeros

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(number):
+    n, n1, n2 = 0, 0, 1
+    FibonacciListNumber = [0, 1]
+    while n<number:
+        n = n1+n2
+        n1 = n2
+        n2 = n
+        if n <= number:
+            FibonacciListNumber.append(n)
+
+    print('La lista de numeros de Fibonacci es de {} y son los siguientes: '.format(len(FibonacciListNumber)), FibonacciListNumber)
+
+fibonacci(1000)
+    
